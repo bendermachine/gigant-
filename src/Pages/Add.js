10232ba29ea82355ee38cb79a0a38cd5f8bf5055
Daniil Petrenko <daniilp162@gmail.com>
2023-05-23 14:03:03 +0500

    kek

--- src/Pages/Add.js
+++ src/Pages/Add.js
@@ -1,13 +1,122 @@
 import React, {Component} from 'react';
+import '../App.css'
+import '../add.css'
+import {Form, FormControl} from "react-bootstrap";
 
 class Add extends Component {
+
     render() {
         return (
-            <div>
-                <h1>Привет</h1>
+<div>
+    <div
+        style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            paddingTop:'5.5%',
+            margin:'auto',
+        }}>
+        <p
+            style={{
+                fontSize:25,
+            }}
+        >Добавить объявление</p>
+    </div>
+<div style={{
+    backgroundColor: 'white',
+    width: "60%",
+    height: "80%",
+    position: 'absolute',
+    marginTop:'3%',
+    top: '10%',
+    left: '20%',
+    overflow:' auto',
+    }}>
+    <Form inline>
+        <FormControl
+            type="text"
+            placeholder="Название группы"
+            className="mr-sm-2"
+            style = {{
+                width:'300px',
+                margin:'auto',
+                display: 'flex',
+                marginTop:'17px',
+                marginBottom: '17px',
+                padding: '6px 13px 6px 13px'
+        }}
+        />
+    </Form>
+
+
+    <hr style = {{width:"95%", margin: 'auto'}}></hr>
+    <div className="wrapper">
+        <div>
+            <div className="mb-4 d-flex justify-content-center">
+                <img src="../components/photo_logo.svg"/>
             </div>
+            <div className="d-flex justify-content-center">
+                <div className="btn btn-primary btn-rounded">
+                    <label className="form-label text-white m-1" htmlFor="customFile1">Choose file</label>
+                    <input type="file" className="form-control d-none" id="customFile1"/>
+                </div>
+            </div>
+        </div>
+        <Form inline>
+            <FormControl
+                type="text"
+                placeholder="Название группы"
+                className="mr-sm-2"
+                style = {{
+                    width:'300px',
+                    margin:'auto',
+                    display: 'flex',
+                    marginTop:'17px',
+                    marginBottom: '17px',
+                    padding: '6px 13px 6px 13px'
+                }}
+            />
+        </Form>
+        <Form inline>
+            <FormControl
+                type="text"
+                placeholder="Название группы"
+                className="mr-sm-2"
+                style = {{
+                    width:'300px',
+                    margin:'auto',
+                    display: 'flex',
+                    marginTop:'17px',
+                    marginBottom: '17px',
+                    padding: '6px 13px 6px 13px'
+                }}
+            />
+        </Form>
+        <Form inline>
+            <FormControl
+                type="text"
+                placeholder="Название группы"
+                className="mr-sm-2"
+                style = {{
+                    width:'300px',
+                    margin:'auto',
+                    display: 'flex',
+                    marginTop:'17px',
+                    marginBottom: '17px',
+                    padding: '6px 13px 6px 13px'
+                }}
+            />
+        </Form>
+
+
+    </div>
+
+</div>
+
+</div>
         );
     }
 }
+
 
 export default Add;
---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import {Button, Container, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import logo from './ant_logo.png'
+import { Route, Routes} from "react-router-dom"
+
+import Home from "../Pages/Home";
+import Add from "../Pages/Add";
+import Description from "../Pages/Description";
+import Account from "../Pages/account";
+
+class Header extends Component {
+    render() {
+        return (
+            <>
+            <Navbar fixed={"top"} collapseOnSelect expand="md" bg = "light" variant="light">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            src = {logo}
+                            height="60"
+                            width="180"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                            />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href={'/'}> Объявления </Nav.Link>
+                            <Nav.Link href={'/Add'}> Добавить объявление </Nav.Link>
+                            <Nav.Link href={'/Account'}> Личный кабинет </Nav.Link>
+
+                        </Nav>
+                        <Form inline>
+                            <FormControl
+                            type="text"
+                            placeholder="Search"
+                            className="mr-sm-2"
+                            />
+                            {/*<Button variant={"outline-info"}>Search</Button>*/}
+                        </Form>
+                        <Button variant={"outline-info"}>Search</Button>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            {/*<Routes>*/}
+            {/*<Route path='/home' element={<Home />}/>*/}
+            {/*<Route path='/add' element={<Add/>}/>*/}
+            {/*</Routes>*/}
+
+               <Routes>
+                   <Route path="/" element={<Home />} />
+                   <Route path="/Add" element={<Add/>} />
+                   <Route path={"/Description"} element={<Description/>} />
+                   <Route path={"/Account"} element={<Account/>} />
+               </Routes>
+
+    </>
+        );
+    }
+}
+
+export default Header;
