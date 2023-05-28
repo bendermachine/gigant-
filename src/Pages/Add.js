e20793de3dbd0b2d14d6115941b19cb6582cf991
Daniil Petrenko <daniilp162@gmail.com>
2023-05-28 17:06:16 +0500

    important commit

--- src/Pages/Add.js
+++ src/Pages/Add.js
@@ -6,16 +6,53 @@ import photo from '../components/photo_logo.svg'
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import { Calendar } from "react-modern-calendar-datepicker";
 import DatePicker from "react-modern-calendar-datepicker";
-
+import {render} from "@testing-library/react";
+import {ImageUploader} from './handleImageUpload.js'
 const defaultValue = {
     year: 2023,
     month: 4,
     day: 20,
 };
 
+
 function Add() {
     const [selectedDay, setSelectedDay] = useState(defaultValue);
+    const [preview, setPreview] = useState(photo);
 
+
+
+        const handleImageUpload = (event) => {
+            const file = event.target.files[0];
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                setPreview(reader.result);
+            };
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        };
+
+
+
+        // function ImageUploader(){
+    //     const [preview, setPreview] = useState(null);
+    //
+    //     const handleImageUpload = (event) => {
+    //         const file = event.target.files[0];
+    //         const reader = new FileReader();
+    //
+    //         reader.onloadend = () => {
+    //             setPreview(reader.result);
+    //         };
+    //
+    //         if (file) {
+    //             reader.readAsDataURL(file);
+    //         }
+    //     }
+    // }
+    // let preview;
     return (
         <div>
             <div
@@ -50,12 +87,14 @@ function Add() {
                 <div className="wrapper">
                     <div className={"block"} style={{height:"583px", paddingTop:"30%"}}>
                         <div className="mb-4 d-flex justify-content-center">
-                            <img src={photo} alt="Прикрепите фотографии"/>
+                            {/*<img src={photo} alt="Прикрепите фотографии"/>*/}
                         </div>
                         <div className="d-flex justify-content-center">
                             <div className="btn btn-primary btn-rounded" style={{backgroundColor:'gray', border:'0px'}}>
                                 <label className="form-label text-white m-1" htmlFor="customFile1">Прикрепить фотографии</label>
                                 <input type="file" className="form-control d-none" id="customFile1"/>
+                                <input type="file" accept="image/*" onChange={handleImageUpload} />
+                                {preview && <img src={preview} style={{ width: 200, height: 150,margin:10}} alt="Preview" />}
                             </div>
                         </div>
                     </div>
@@ -113,3 +152,4 @@ function Add() {
 }
 
 export default Add;
+// export default HandleImageUpload;
